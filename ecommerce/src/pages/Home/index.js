2c437451715789c55c0cabbd72f012d1a36eb544
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-26 17:41:53 -0300

    refactoring page home

--- ecommerce/src/pages/Home/index.js
+++ ecommerce/src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Header } from 'react-native-elements';
-
-export default class Home extends React.Component {
+import { connect } from 'react-redux';
+import { LABELS } from '../../settings/labels';
+import { styles } from './style';
+class Home extends React.Component {
     constructor(props) {
         super(props);
 
@@ -13,10 +15,27 @@ export default class Home extends React.Component {
     render() {
         return (
             <Header
-                leftComponent={{ icon: 'menu', color: '#fff' }}
-                centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
+                centerComponent={
+                    { 
+                        text: LABELS.Header.title,
+                        style: {
+                            color: '#fff'
+                        }
+                    }
+                }
+                rightComponent={
+                    {
+                        icon: 'home',
+                        color: '#fff' 
+                    }
+                }
             />
         )
     }
 }
+const mapStateToProps = state => (
+    {
+        state: state
+    }
+)
+export default connect(mapStateToProps)(Home)
