036fc96038a7c0aafeccba6a8371a0e127f40f7a
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-26 22:22:40 -0300

    product component

---
+++ ecommerce/src/components/Product/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card } from 'react-native-elements';
+import { View, Text, Button } from 'react-native';
+
+export default class Product extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Card title="PS4">
+                <View>
+                    <Text>{this.props.quantity}: 5</Text>
+                    <Text>{this.props.price}: 5</Text>
+                    <Text>{this.props.description}: Um video game foda</Text>
+                    <Button 
+                        title={this.props.labelButton}
+                        onPress={() => alert('adicionado ao carrinho')}
+                    />
+                </View>
+            </Card>
+        )
+    }
+}
+
+Product.defaultProps = {
+    quantity: 'Quantidade',
+    price: 'Preço',
+    description: 'Descrição',
+    labelButton: 'adicionar ao carrinho'
+}
