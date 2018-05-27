f9c43bc4c6fe424b3ecf8b13dcf0d864cb824e9e
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-27 15:58:52 -0300

    adding scroll view

--- ecommerce/src/components/ProductList/index.js
+++ ecommerce/src/components/ProductList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { FlatList } from 'react-native';
 import { Actions } from 'react-native-router-flux';
@@ -17,20 +18,22 @@ export default class ProductList extends React.Component {
 
     render() {
         return (
-            <Card containerStyle={styles.container}>
-                <FlatList
-                    data={this.props.products}
-                    extraData={this.state}
-                    keyExtractor={this.keyExtractor}
-                    renderItem={({item}) => 
-                        <ListItem
-                            key={item.id}
-                            title={item.name}
-                            onPress={() => Actions.product({item, remove:this.props.remove})}
-                        />
-                    }
-                />
-            </Card>
+            <ScrollView>
+                <Card containerStyle={styles.container}>
+                    <FlatList
+                        data={this.props.products}
+                        extraData={this.state}
+                        keyExtractor={this.keyExtractor}
+                        renderItem={({item}) => 
+                            <ListItem
+                                key={item.id}
+                                title={item.name}
+                                onPress={() => Actions.product({item, remove:this.props.remove})}
+                            />
+                        }
+                    />
+                </Card>
+            </ScrollView>
         )
     }
 }
